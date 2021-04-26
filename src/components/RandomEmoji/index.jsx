1e501e3ada64a7cb2f71d21c2d2b286bded9c32a
pedroalles <pedroalles@gmail.com>
2021-04-26 02:59:47 -0300

    Update index.jsx

--- src/components/RandomEmoji/index.jsx
+++ src/components/RandomEmoji/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-const emojis = "📀 💽 💾 💿 💻 📱 📊 📚 📖 📘 📂 ⌨ 🖱".split(" ") 
+const emojis = "💽 💾 💿 💻 📱 📊 📚 📖 📘 📂 ⌨ 🖱 🕹".split(" ") 
 
 const RandomEmoji = () =>{
     const [emoji, setEmoji] = useState(emojis[Math.floor(Math.random() * emojis.length)])
